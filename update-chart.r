2fcabba76308d5b07f23ec8e4ae1d4a4e5263395
Chris P <christopher.mark.persaud@gmail.com>
2021-08-21 14:23:06 -0400

    add chart updater

---
+++ update-chart.r
@@ -0,0 +1,48 @@
+source('fl-cases-deaths-7-day-avg.r')
+# devtools::install_github("munichrocker/DatawRappr",ref="master")
+
+library(DatawRappr)
+
+print("Starting chart updater")
+
+latestdate <- max(fl$submission_date_formatted)
+
+latestdate <- as.POSIXct(
+  x = max(fl$submission_date_formatted),
+  format = "%Y-%m-%d"
+)
+
+updateDateFormat <- gsub(
+  pattern = " 0",
+  replacement = ' ',
+  x = format(
+    x = latestdate,
+    format = "%B %d, %Y"
+  )
+)
+
+chartIDs <- c(
+  'gOqmg', # total cases
+  'IO4D3', # new cases
+  'B18iA', # total deaths
+  'ULrmv' # new deaths
+)
+
+apikey <- Sys.getenv("DATAWRAPPER_API")
+
+for (id in chartIDs) {
+  dw_edit_chart(
+    chart_id = id,
+    api_key = apikey,
+    annotate = paste0("Updated ",updateDateFormat,'.')
+  )
+  print("Publishing chart")  
+  dw_publish_chart(
+    chart_id = id,
+    api_key = apikey,
+    return_urls = TRUE
+  )  
+}
+
+
+
